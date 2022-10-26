8971d96779000382c54a0cc123ba024e2f70924a
fahim ashhab <37286610+fahim-ash@users.noreply.github.com>
2022-10-26 20:03:25 +0600

    Add files via upload

--- mernApp/client/index.js
+++ mernApp/client/index.js
@@ -1,16 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const express=require("express");
+const app=express();
+const mongoose=require("mongoose");
+const UserModel=require('./models/Users');
+const cors=require('cors')
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+app.use(express.json())
+app.use(cors());
+
+mongoose.connect("mongodb+srv://ash:<Password>@cluster0.tcuoysi.mongodb.net/mernDB?retryWrites=true&w=majority")
+
+
+app.get("/getUsers",(req,res)=>{
+    UserModel.find({},(err,result)=>{
+        if(err){
+            res.json(err);
+
+        }else{
+            res.json(result);
+        }
+
+    });
+
+})
+
+app.post("/createUser",async(req,res)=>{
+    const user=req.body;
+    const newUser=new UserModel(user);
+    await newUser.save();
+
+    res.json(user);
+
+
+
+})
+
+
+app.listen(3001,()=>{
+    console.log("app is running on port 3001")
+
+});
